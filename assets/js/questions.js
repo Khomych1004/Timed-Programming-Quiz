e28b1787b34442ca07a213445b3f9d63d60f8f48
xoma88 <homich8897@gmail.com>
2024-01-16 03:01:26 +0000

    added array with questions

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question:"Commonly used data types DO Not Include:",
+        answers: [
+            "strings", 
+            "booleans", 
+            "alerts", 
+            "numbers"
+        ],
+        correct: 0
+    },
+    {
+        question:"The condition in an if / else statement is enclosed with ___________.",
+        answers: [
+            "quotes", 
+            "curly brackets", 
+            "parenthesis", 
+            "square brackets"
+        ],
+        correct: 2
+    },
+    {
+        question:"Arrays in JavaScript can be used to store _________.",
+        answers: [
+            "numbers and strings", 
+            "other arrays", 
+            "booleans", 
+            "all of the above"
+        ],
+        correct: 3
+    },
+    {
+        question:"String values must be enclosed within ______ when being assigned to variables.",
+        answers: [
+            "commas", 
+            "curly brackets", 
+            "quotes", 
+            "parenthesis"
+        ],
+        correct: 0
+    },
+    {
+        question:"A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            "JavaScript", 
+            "terminal/bash", 
+            "for loops", 
+            "console.log"
+        ],
+        correct: 0
+    }
+]
