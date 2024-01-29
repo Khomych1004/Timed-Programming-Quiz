ae4898b1ab338ea5ea2d84729c02f916deff1d92
xoma88 <homich8897@gmail.com>
2024-01-29 23:59:37 +0000

    creating all the variables to work with

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,23 @@
 const startScreen = document.querySelector('#start-screen');
-const endScreen = document.querySelector('#end-screen');
 const questionsScreen = document.querySelector('#questions');
+const endScreen = document.querySelector('#end-screen');
+const choices = document.querySelector('#choices');
+const timeBlock = document.querySelector('#time');
+const finalScore = document.querySelector('#final-score');
+const btnSubmit = document.querySelector('#submit');
+const initials = document.querySelector('#initials');
 const btnStart = startScreen.querySelector('#start');
 
+let line = document.createElement('hr')
+let p = document.createElement('p')
+
+var time = 75;
+var timerID;
+var pauseId;
+var questionNumber = 0;
+var numberQuestions = helpersQuestions.getNumberQuestions();
+
+var players = [];
 
 btnStart.addEventListener('click', function(){
     //startScreen.setAttribute("style", "display: none");
