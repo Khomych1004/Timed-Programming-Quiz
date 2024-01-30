abe6c46599d0bf6ca08d6ab94bcc1617fe567e3e
xoma88 <homich8897@gmail.com>
2024-01-30 00:08:45 +0000

    adding a handler for the game start button, starting a timer and displaying questions on the screen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -19,12 +19,90 @@ var numberQuestions = helpersQuestions.getNumberQuestions();
 
 var players = [];
 
-btnStart.addEventListener('click', function(){
-    //startScreen.setAttribute("style", "display: none");
-    //questionsScreen.setAttribute("style", "display: block");
+//creating an answer option button
+function createButton(id, text) {
 
+    let button = document.getElementById(id);
+    var str = `${id + 1 + '. '}`;
+  
+    if (button) {
+  
+      button.textContent = str + text;
+  
+    } else {
+  
+      //creating a button with an answer option
+      button = document.createElement('button');
+      button.id = id;
+      button.textContent = str + text;
+  
+      //handler for clicking the response button, checking the response
+      button.addEventListener('click', function () {
+  
+        var answer = "";
+  
+        //check for correct answer
+        answer = checkingAnswer(helpersQuestions.checkingAnswer(questionNumber), this.id);
+  
+        //move to next question
+        nextQuestionScreen(answer);
+  
+      });
+  
+      //adds a button with an answer option to the screen
+      choices.append(button);
+    }
+  };
+  
+  //game time countdown starts
+  function startTime() {
+  
+    clearInterval(timerID);
+    --time;
+  
+    timerID = setInterval(function () {
+  
+      if (time < 10) {
+  
+        if (time < 0) {
+  
+          timeBlock.textContent = "00";
+          openEndScreen();
+  
+        } else {
+  
+          timeBlock.textContent = "0" + time--;
+  
+        }
+      } else {
+  
+        timeBlock.textContent = time--;
+  
+      }
+    }, 1000);
+  };
+  
+  //displays the question and answer options
+  function logic() {
+  
+    //question
+    questionsScreen.querySelector('#question-title').textContent = helpersQuestions.getQuestion(questionNumber);
+    //answer options
+    helpersQuestions.getAnswer(questionNumber).forEach((answer, idx) => createButton(idx, answer));
+  
+  };
+  
+  //function processing by pressing the start button
+  btnStart.addEventListener('click', function () {
+  
+    timeBlock.textContent = time;
+  
+    startTime();
+  
     startScreen.style.display = 'none';
     questionsScreen.style.display = 'block';
-    
-    questionsScreen.querySelector('#question-title').textContent = questions[0].question;
-});
+  
+    logic();
+  
+  });
+  
