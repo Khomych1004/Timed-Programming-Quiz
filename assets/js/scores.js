4fafe6a5917f5adcb95503a8f7893034addc91a3
xoma88 <homich8897@gmail.com>
2024-01-30 00:24:08 +0000

    player display function

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,2 +1,22 @@
 const highscores = document.querySelector('#highscores');
 const clear = document.querySelector('#clear');
+
+dataFilling();
+
+//player display function
+function dataFilling() {
+    
+    var storedPlayers = JSON.parse(localStorage.getItem("players"));
+
+    if (storedPlayers !== null) {
+        
+        storedPlayers.forEach((player) => {
+
+            li = document.createElement('li');
+            li.innerHTML = `${player.name + ' - ' + player.time}`;
+
+            highscores.append(li);
+
+        });
+    };
+};
